65fad928881abbfa40ef7141de3c30c9d52badd0
RodrigoMuinhos <rodrigomuinhostattooist@gmail.com>
2026-07-25 13:54:59 -0300

    atualiza configuração do projeto

--- backend/src/main/java/com/isismariana/backend/config/WebConfig.java
+++ backend/src/main/java/com/isismariana/backend/config/WebConfig.java
@@ -1,10 +1,10 @@
 package com.isismariana.backend.config;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.Arrays;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
